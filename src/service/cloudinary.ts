64593419fbbed8190d4b6f617588d8de58fdfd63
Sagar Chakrawarti <sagarchakrawarti25@gmail.com>
2024-11-12 22:05:18 +0530

    update service url

--- src/service/cloudinary.ts
+++ src/service/cloudinary.ts
@@ -1,2 +1,6 @@
-export const cloud_name = "dvslu2jt5"
-export const upload_preset = "q6dcnbc0"
+import dotenv from 'dotenv';
+
+
+dotenv.config();
+export const cloud_name = process.env.CLOUD_NAME;
+export const upload_preset = process.env.UPLOAD_PRESET;
